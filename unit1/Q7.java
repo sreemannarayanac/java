62344f3ebd7e0d58d4040fb4edccb909f75551b6
Chamarti Sreemannarayana <93900514+sreemannarayanac@users.noreply.github.com>
2023-02-07 09:08:47 +0530

    Added Q7

---
+++ unit1/Q7.java
@@ -0,0 +1,13 @@
+// Program to display first 10 natural numbers
+
+import java.util.Scanner;
+
+public class Q7 {
+	public static void main (String args[]) {
+		Scanner inp = new Scanner(System.in);
+		System.out.println("First 10 natural numbers are: ");
+		for (int i=0; i<10; i++) {
+			System.out.print(i+1 + " ");
+		}
+	}
+}
