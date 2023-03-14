d3ceaa0030e19347520716487f34e622019edc04
Chamarti Sreemannarayana <93900514+sreemannarayanac@users.noreply.github.com>
2023-03-14 08:28:59 +0530

    Update Q7.java

--- unit2/Q7.java
+++ unit2/Q7.java
@@ -1,16 +1,22 @@
 // Illustrate the usage of 'this', 'final' and 'finalize' usinga a java program.
 
-class Q7 {
+class Main {
     int price = 1000;
     final int tax = 10;
+    void getPrice(int price){
+        System.out.println("Price passed as argument: "+price);
+        System.out.println("Price passed of Object: "+this.price);
+    }
     public static void main(String args[]) {
-        Q7 obj = new Q7();
+        Main obj = new Main();
         System.out.println("Price is: " + obj.price);
         System.out.println("Tax is: " + obj.tax);
         obj.price = 2000;
         // obj.tax = 20; // error: cannot assign a value to final variable tax
         System.out.println("Price is: " + obj.price);
         System.out.println("Tax is: " + obj.tax);
+        System.out.println("");
+        obj.getPrice(13435);
         obj.finalize();
         obj = null;
         System.gc();
